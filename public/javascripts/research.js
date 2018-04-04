9caeb50057ebd4c3b44e393b314728b48e38da05
derek <derekhuynh64@gmail.com>
2018-04-04 12:42:52 -0400

    add sub title to research entries

--- public/javascripts/research.js
+++ public/javascripts/research.js
@@ -3,9 +3,8 @@ var documents = [];
 $(document).ready(function(){
   //TODO: get rid of this and do it with ejs and express same for team
   $.getJSON("/json/research.json",function(data){
-    console.log("test2");
     for (var i = 0; i < data.research.length; i++) {
-      console.log(data.research.length);
+      //console.log(data.research.length);
       var r = new createResearch(data.research[i])
       documents.push(r);
       //createResearch(data.research[i]);
@@ -25,6 +24,7 @@ $(document).ready(function(){
     let doca = document.createElement("a");
     let docInnerContainer = document.createElement("div");
     let docTitle = document.createElement("h4");
+    let docSubTitle = document.createElement("h5");
     let docImg = document.createElement("img");
     let docImgContainer = document.createElement("div");
 
@@ -33,6 +33,7 @@ $(document).ready(function(){
     doca.setAttribute("target","_blank");
     docImg.setAttribute("src","/images/documents/"+d.img);
     docTitle.innerHTML = d.title;
+    docSubTitle.innerHTML = d.subTitle;
     docImgContainer.setAttribute("class", "img-container");
 
     this.doc.appendChild(doca);
@@ -40,6 +41,7 @@ $(document).ready(function(){
     docImgContainer.appendChild(docImg);
     if(!(screen.width <= 699))docInnerContainer.appendChild(docImgContainer);
     docInnerContainer.appendChild(docTitle);
+    docInnerContainer.appendChild(docSubTitle);
 
   }
   function filterDocuments(type){
