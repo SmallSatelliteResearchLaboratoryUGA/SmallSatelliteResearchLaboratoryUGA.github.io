cb697939b635c86eef5fa24859567e3ae322e108
caleb adams <piepieninja@gmail.com>
2019-06-20 12:56:44 -0400

    doin the lords work

--- public/javascripts/team.js
+++ public/javascripts/team.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
   $.getJSON("/json/team.json",function(data){
     //populate principleinvestigators
     for (let i = 0; i < data.team.principleinvestigators.length; i++) {
-      createMember(data.team.principleinvestigators[i], "#section-principleinvestigators");
+      createMemberExtraContent(data.team.principleinvestigators[i], "#section-principleinvestigators");
     }
     //populate graduatestudents
     for (let i = 0; i < data.team.graduatestudents.length; i++) {
-      createMember(data.team.graduatestudents[i], "#section-graduatestudents");
+      createMemberExtraContent(data.team.graduatestudents[i], "#section-graduatestudents");
     }
     //populate leadership
     for (let i = 0; i < data.team.leadership.length; i++) {
@@ -47,6 +47,69 @@ $(document).ready(function(){
   });
 });
 
+// this creates a member and also contains extra
+// content about the member. This will have a CV
+// link and
+// This is expected to be done for faculty and grad students.
+function createMemberExtraContent(member, sectionid){
+  var d = member;
+  var profileBoi = document.createElement("span")
+  // var profilelink = document.createElement("a");
+  //
+  // //set link if json has 'link' key
+  // if (d.hasOwnProperty("link")) {
+  //   profilelink.setAttribute("href",d.link);
+  // }
+
+  var profileHeader = document.createElement("div");
+  profileHeader.setAttribute("class", "profile");
+  var profileHeaderImage = document.createElement("img");
+
+  //get image, if none then use default
+  if (d.img === "") {
+    profileHeaderImage.setAttribute("src", "/images/SSRLProfiles/default.png");
+  }else {
+    profileHeaderImage.setAttribute("src", "/images/SSRLProfiles/" + d.img);
+  }
+
+  var profileHeaderName = document.createElement("span");
+  profileHeaderName.setAttribute("class", "name");
+  profileHeaderName.innerHTML = d.name;
+
+  //append elements
+  profileHeader.appendChild(profileHeaderImage);
+  profileHeader.appendChild(profileHeaderName);
+  if (!(d.role === "")) {
+    let profileHeaderTitle = document.createElement("span");
+    profileHeaderTitle.setAttribute("class", "title");
+    profileHeaderTitle.innerHTML = d.role + '<br><br>';
+    // add the extra boi if it exists
+    if (d.hasOwnProperty("link")) {
+      var cvlink = document.createElement("a");
+      cvlink.innerHTML += '<i class="far fa-file-alt"></i> - ';
+      // because of grammar and spelling and stuff we have
+      // to put the apostropheeee guy in the right spot
+      var last = d.name[d.name.length -1];
+      if (last === "s" || last === "S") {
+        cvlink.innerHTML += d.name + '\' CV'
+      } else {
+        cvlink.innerHTML += d.name + '\'s CV'
+      }
+      cvlink.setAttribute("href",d.link);
+      cvlink.setAttribute("target",'_blank');
+      profileHeaderTitle.appendChild(cvlink)
+    }
+    profileHeader.appendChild(profileHeaderTitle);
+  }
+  profileBoi.appendChild(profileHeader)
+  // profilelink.appendChild(profileHeader);
+  $(sectionid).append(profileBoi);
+
+}
+
+// this function is a standard member
+// this does not include any extra links
+// or any extra content about said member
 function createMember(member, sectionid){
   var d = member;
   var profilelink = document.createElement("a");
