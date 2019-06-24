4d7c92d870698acf06bc95374eca3908cc4d206c
Derek <derekhuynh64@gmail.com>
2019-06-24 16:57:30 -0400

    cleaned up outreach page
    #3
    also organizes some styles

--- public/javascripts/outreach.js
+++ public/javascripts/outreach.js
@@ -14,29 +14,28 @@ function createEvent(eventObject){
   //create elements
   let rule = document.createElement("hr");
   let container = document.createElement("div");
-  let title = document.createElement("h3");
+  let title = document.createElement("h1");
   let subTitle = document.createElement("p");
   let descContainer = document.createElement("div");
   let imgContainer = document.createElement("div");
-  let img = document.createElement("img");
   let description = document.createElement("p");
   //modify elements
   container.setAttribute("class","outreach");
   descContainer.setAttribute("class","description");
   imgContainer.setAttribute("class","outreach-img");
-  img.setAttribute("src",e.img);
+  imgContainer.setAttribute("style","background-image: url(\'" + e.img + "\')");
   title.innerHTML = e.title;
   description.innerHTML = e.description;
   subTitle.innerHTML = '<p class="sub-title">Date: </p> <p class="sub">' + e.date + '</p> <p class="sub-title">location: </p> <p class="sub">' + e.location +
     '</p> <p class="sub-title">age: </p> <p class="sub">' + e.age + '</p> <p class="sub-title">number of students: </p> <p class="sub">' + e.studentAmount + '</p>';
   //put things together
-  container.appendChild(rule);
-  container.appendChild(title);
-  container.appendChild(subTitle);
+  $("#events").append(rule);
   container.appendChild(descContainer);
-  descContainer.appendChild(imgContainer);
+  container.appendChild(imgContainer);
+  container.appendChild(descContainer);
+  descContainer.appendChild(title);
+  descContainer.appendChild(subTitle);
   descContainer.appendChild(description);
-  imgContainer.appendChild(img);
   $("#events").append(container);
 }
 
