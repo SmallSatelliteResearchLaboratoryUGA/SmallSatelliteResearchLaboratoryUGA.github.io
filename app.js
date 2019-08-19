16fa58671189e89ea878bc79b6a54f95b4dba4f5
caleb adams <piepieninja@gmail.com>
2019-08-19 16:11:11 -0400

    fixes apply link

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.get('/missions', function (req, res) {
   res.render('missions');
 })
 app.get('/apply', function (req, res) {
-  res.redirect('https://piepieninja.typeform.com/to/MAnpLo');
+  res.redirect('https://docs.google.com/forms/d/e/1FAIpQLSdLu_PnAXqS3KPzwiZA1X70kq-SHOUKztIrlxH_EuQ5H-cYTw/viewform?usp=sf_link');
 })
 app.get('/partners', function (req, res) {
   res.render('partners');
